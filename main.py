b906cd62ef04fa0fc83f130f7db1caae40ffc602
cesardgm <cesardgm@berkeley.edu>
2023-06-07 13:40:02 -0700

    Update Interactive Text Editor (ine) from version 1.0 to 2.0
    - Refactored global variables: Simplified global space, removed unnecessary ones for cleaner state.
    - Modularized code: Broke main loop into specific functions for each operation.
    - Improved user interface: Removed constant printing of options.
    - Error handling: Added error handling for file opening, line editing, and deleting.
    - Added delete feature: Users can now delete specific lines or a range.
    - Added insert feature: Users can now insert lines anywhere in the file.
    - Improved file saving: Optimized file saving mechanism.
    - Added console clearing: The console is now cleared after each operation.
    - Error logging: Errors are now logged to a separate file.
    - Removed unused functionality: Removed "view" option as file content is always displayed.
    - Enhanced user prompts: Refined user prompts for better clarity.
    - Color-coded output: Implemented color-coded console output.
    - File closure: Added proper file closure after editing.
    - Enhanced validation: Improved input validation for edit and delete operations.
    - General code clean up: Removed unnecessary `continue` statements, unnecessary string conversions.

--- main.py
+++ main.py
@@ -1,73 +1,166 @@
+import os
+
+# Global variables
 current_file_path = None
 file_open = None
-file_contents = None
 file_contents_arr = None
-line_number = None
-choice = None 
-stri = ''
 
-def main():
-    global current_file_path, file_open, file_contents, line_number, stri
-    current_file_path = str(input("Enter file path:- "))
+# ANSI color codes
+COLORS = {
+    'yellow': '\033[93m',  # Options
+    'red': '\033[91m',  # Error messages
+    'reset': '\033[0m'  # Reset color
+}
+
+def clear_console():
+    """Clears the console"""
+    if os.name == 'nt':
+        os.system('cls')
+    else:
+        os.system('clear')
+
+def open_file():
+    """
+    Prompts user for file path and opens the file for editing.
+    """
+    global current_file_path, file_open, file_contents_arr
+    current_file_path = str(input("\nEnter file path: "))
     try:
         file_open = open(current_file_path, "r+")
-    except UnicodeDecodeError:
-        print("invalid text file")
-    except FileNotFoundError:
-        print("file not found")
+    except (UnicodeDecodeError, FileNotFoundError):
+        print_error("Unable to open file. Please ensure the file path is correct and the file is a text file.")
+        return False
 
-    file_contents = file_open.read()
-    file_contents_arr = file_contents.splitlines()
-    print("\nfile preview:- \n")
-    for i in range(0, len(file_contents_arr)):
-        print(i+1, file_contents_arr[i])
+    file_contents_arr = file_open.read().splitlines()
+    return True
 
+def close_file():
+    """Closes the current file"""
+    global file_open
+    file_open.close()
+
+def save_file():
+    """
+    Saves the current contents of the file.
+    """
+    global file_open, file_contents_arr
+    file_open.seek(0)
+    file_open.write("\n".join(file_contents_arr))
+    file_open.truncate()
+
+def preview_file():
+    """
+    Prints the current contents of the file.
+    """
+    global file_contents_arr
+    for i, line in enumerate(file_contents_arr, start=1):
+        print(f"{i} {line}")
+
+def add_line():
+    """
+    Adds a new line to the end of the file.
+    """
+    global file_contents_arr
+    file_contents_arr.append(str(input("Enter new line: ")))
+
+def insert_line():
+    """
+    Inserts a new line at a specified line number in the file.
+    """
+    global file_contents_arr
+    try:
+        line_number = int(input("Enter line number to insert at: "))
+        if line_number < 1 or line_number > len(file_contents_arr) + 1:
+            print_error("Invalid line number.")
+            return
+        new_line = str(input("Enter new line: "))
+        file_contents_arr.insert(line_number - 1, new_line)
+    except ValueError:
+        print_error("Invalid input. Please enter a line number.")
+
+def edit_line():
+    """
+    Prompts user for line number and new line content,
+    then replaces specified line with new content.
+    """
+    global file_contents_arr
+    try:
+        line_number = int(input("Enter line number to edit: "))
+        if line_number < 1 or line_number > len(file_contents_arr):
+            print_error("Invalid line number.")
+            return
+        file_contents_arr[line_number-1] = str(input("Enter new line: "))
+    except ValueError:
+        print_error("Invalid input. Please enter a line number.")
+
+def delete_line():
+    """
+    Prompts user for a line or range of lines to delete, then deletes those lines.
+    """
+    global file_contents_arr
+    try:
+        line_range = input("Enter line or range to delete (e.g. 1 or [1:3]): ")
+        if ":" in line_range:  # If input is a range of lines
+            start, end = (int(x) for x in line_range.strip("[]").split(":"))
+            if start < 1 or end > len(file_contents_arr) or start > end:
+                print_error("Invalid range.")
+                return
+            del file_contents_arr[start-1:end]
+        else:  # If input is a single line
+            line_number = int(line_range)
+            if line_number < 1 or line_number > len(file_contents_arr):
+                print_error("Invalid line number.")
+                return
+            del file_contents_arr[line_number-1]
+    except (ValueError, IndexError):
+        print_error("Invalid input. Please enter a line number or range of lines.")
+
+def print_error(message):
+    """
+    Prints the error message and logs it to the error log file.
+    """
+    global current_file_path
+    print(f"{COLORS['red']}{message}{COLORS['reset']}")
+    if current_file_path is not None:
+        with open(f"{current_file_path.rsplit('.', 1)[0]}_err.log", 'a') as err_file:
+            err_file.write(f"{message}\n")
+
+def main():
+    """
+    Main loop of the text editor.
+    """
+    global file_contents_arr
+    if not open_file():
+        return
     while True:
-        print("[a] add new line")
-        print("[e] edit ")
-        print("[s] save")
-        print("[v] view")
-        print("[x] exit")
-        choice = str(input("enter choice:- "))
+        clear_console()
+        preview_file()
+        print(f"\n{COLORS['yellow']}[a] Add [i] Insert [e] Edit [s] Save [d] Delete [x] Exit{COLORS['reset']}")
+        choice = input("Enter choice: ")
         if choice == "x":
             break
         elif choice == "s":
-            stri =''
-            for i in range(0 , len(file_contents_arr)):
-                if i == 0:
-                    stri = stri+file_contents_arr[i]
-                else:
-                    stri = stri+"\n"+file_contents_arr[i]
-
-            file_open.seek(0)
-            file_open.write(stri)
-            file_open.truncate()
-            continue
+            save_file()
         elif choice == "e":
-            print("this file has", len(file_contents_arr), "lines")
-            line_number = int(input("which line you want to edit? "))
-            print('"',file_contents_arr[line_number-1],'"', "is the line you want to edit")
-            new_line = str(input("enter new value:- "))
-            file_contents_arr[line_number-1] = new_line
-            continue
-        elif choice == "v":
-            for i in range(0, len(file_contents_arr)):
-                print(i+1, file_contents_arr[i])
+            edit_line()
         elif choice == "a":
-            file_contents_arr.append(str(input("new line:- ")))
+            add_line()
+        elif choice == "i":
+            insert_line()
+        elif choice == "d":
+            delete_line()
 
 if __name__ == "__main__":
-    print("welcome to ine")
+    print("Welcome to ine")
     while True:
-        print("[o] open a file")
-        print("[e] exiting a file")
-        choice = str(input("enter choice:- "))
+        print(f"\n{COLORS['yellow']}[o] Open a file [e] Exit{COLORS['reset']}")
+        choice = input("Enter choice: ")
         if choice == "o":
             main()
+            close_file()
             break
         elif choice == "e":
             exit()
         else:
-            print("invalid choice")
-            continue
-        
+            print_error("Invalid choice.")
+
